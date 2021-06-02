3e4430cfae4d72e5c4da4e90cb1e0b23b8c62636
vanshika1110 <vanshika1110.cse18@chitkara.edu.in>
2021-06-02 13:06:54 +0530

    myProject

---
+++ build/precache-manifest.7d955496f6c0483efa4f9e294162d6d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "913156ac956250ec3873",
+    "url": "/static/css/main.73a78a9d.chunk.css"
+  },
+  {
+    "revision": "913156ac956250ec3873",
+    "url": "/static/js/main.913156ac.chunk.js"
+  },
+  {
+    "revision": "33786c75a12d0d22b9ef",
+    "url": "/static/css/1.1211e233.chunk.css"
+  },
+  {
+    "revision": "33786c75a12d0d22b9ef",
+    "url": "/static/js/1.33786c75.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "1efffefdf079fc349df1d80cdf617b4f",
+    "url": "/index.html"
+  }
+];
